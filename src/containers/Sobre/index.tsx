b95e133471a599c748126ca866faf34c46400024
Humberto Nacif <gigantedeluz@gmail.com>
2023-07-25 15:13:37 -0300

    Creating the grid and main sections

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam officiis
+      saepe, maxime et assumenda suscipit ex velit culpa laudantium enim animi
+      rem recusandae corrupti aspernatur eius hic. Eius, iure quisquam?
+    </p>
+  </section>
+)
+
+export default Sobre
